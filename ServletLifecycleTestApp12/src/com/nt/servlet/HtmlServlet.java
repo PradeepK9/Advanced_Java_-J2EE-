cbb0a092665022eaa3754e3ae2455bfa80d258f9
Pradeep_Jaiswal <pradeepjaiswalsidd@gmail.com>
2020-11-23 23:59:47 +0530

    Servlet life cycle POC

--- ServletLifecycleTestApp12/src/com/nt/servlet/HtmlServlet.java
+++ ServletLifecycleTestApp12/src/com/nt/servlet/HtmlServlet.java
@@ -33,7 +33,7 @@ public class HtmlServlet extends  HttpServlet
 		 //write b.logic 
 		 pw.println("<table border='1'  style='color:red'>");
 		 pw.println("<tr> <th>Cricketer name  </th> <th> Role </th> <th> JersyNo </th> <th> NickName  </th> </tr>");
-         pw.println("<tr> <td>King  Kohli </td> <td> captain </td> <td> 18</td> <td>Run Machine </td> </tr>");
+         pw.println("<tr> <td> Kohli </td> <td> captain </td> <td> 18</td> <td>Run Machine </td> </tr>");
 		 pw.println("<tr> <td> Dhoni </td> <td> Ass.Captain </td> <td> 7</td> <td>Mr.Cool </td> </tr>");
 		 pw.println("<tr> <td> Rohit </td> <td> Vice.Captain </td> <td> 45</td> <td> Hit Man </td> </tr>");
 		 pw.println("<tr> <td> Panth </td> <td> Ameture Batsman </td> <td> 17</td> <td> WrostFellow </td> </tr>");
